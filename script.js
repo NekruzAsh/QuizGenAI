1c6a881ab382625620053c926808c8f7b8342979
Nekruz Ashrapov <64292629+NekruzAsh@users.noreply.github.com>
2023-11-09 15:13:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
-
-    
     var userInput = document.getElementById('user-input');
     var slider = document.getElementById('slider');
     var sliderValueDisplay = document.getElementById('slider-value');
     var generateQuizButton = document.getElementById('generate-quiz');
 
-   
     slider.addEventListener('input', function () {
         var selectedValue = slider.value;
         sliderValueDisplay.textContent = 'Selected value: ' + selectedValue;
@@ -16,20 +12,17 @@ document.addEventListener('DOMContentLoaded', function () {
     generateQuizButton.addEventListener('click', function () {
         var textInput = userInput.value;
         var numberOfQuestions = slider.value;
-    
-       
+
         const promptText = textInput;
         const numQuestions = numberOfQuestions;
         const prompt = `With the following text: ${promptText}\nGenerate a quiz consisting of ${numQuestions}, multiple choice questions with a high difficulty level. Include an answer key at the end of the quiz.\n`;
-    
-        
+
         callApi(textInput, numberOfQuestions, prompt);
     });
 
     function callApi(textInput, numberOfQuestions, prompt) {
-        
         fetch('https://api.cohere.ai/v1/generate', {
-            method: 'POST', 
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'BEARER FUFAxlCrLUL7ZOYSh5RAOFhadrYR99bQbWpaxXsA'
@@ -42,23 +35,40 @@ document.addEventListener('DOMContentLoaded', function () {
                 "k": 0,
                 "stop_sequences": [],
                 "return_likelihoods": "NONE"
-              }),
+            }),
         })
         .then(response => response.json())
         .then(data => {
-            
             console.log('API Response:', data);
-        
+
             if (data.generations && data.generations.length > 0) {
-                document.querySelector('#quiz-display').innerHTML = data.generations[0].text;
+                var quizDisplay = document.querySelector('#quiz-display');
+                quizDisplay.innerHTML = data.generations[0].text;
+
+                // Apply styling to the generated elements
+                var questionContainers = quizDisplay.querySelectorAll('.question-container');
+                questionContainers.forEach(function (container) {
+                    container.style.border = '2px solid #000';
+                    container.style.borderRadius = '5px';
+                    container.style.margin = '15px 0';
+                    container.style.padding = '10px';
+                    container.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.1)';
+
+                    var answerContainers = container.querySelectorAll('.answer-container');
+                    answerContainers.forEach(function (answerContainer) {
+                        answerContainer.style.border = '2px solid #000';
+                        answerContainer.style.borderRadius = '5px';
+                        answerContainer.style.margin = '10px 0';
+                        answerContainer.style.padding = '8px';
+                        answerContainer.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.1)';
+                    });
+                });
             } else {
                 console.error('Error: No generations found in the API response.');
             }
-        
-            
         })
-
-    .catch(error => {
-        console.error('Error:', error);
-    });}
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
 });
